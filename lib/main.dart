f30c61c1fa876e4d1d653ce19a105f52757a0115
Faizan Darwesh <faizandarwesh@outlook.com>
2021-10-02 14:46:58 +0500

    modify app title

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Github Actions',
+      title: 'Flutter CI with Github Actions',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
